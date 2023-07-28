f36477a3dc405ace18c7fbe6f694af6de312d370
Hys-Lee <dltkdgus3275@gmail.com>
2023-07-28 14:49:15 +0900

    new branch

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // 이제는 checkout말고 switch를 이용해서 branch 이동한다고 함.
 function add_test(x,y){
     console.log(x);
@@ -5,3 +6,6 @@ function add_test(x,y){
     console.log(x+y);
     return x+y;
 }
+=======
+asdasd
+>>>>>>> 9b90bbe1264c3f694adf77e4d4a005187f541622
